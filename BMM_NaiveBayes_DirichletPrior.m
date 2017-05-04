aa7cb50534a241025a609f8237ce6b110bde6a61
priyankjaini <pjaini@v1020-wn-226-17.campus-dynamic.uwaterloo.ca>
2017-05-04 10:30:47 -0400

    minor changes

--- BMM_NaiveBayes_DirichletPrior.m
+++ BMM_NaiveBayes_DirichletPrior.m
@@ -1,5 +1,8 @@
 clear all
-numData = 200000;
+numData = 30000;
+%%% Check the running thing of the code with the initial parameters - when
+%%% it gives error?
+
 %%% DEFINE NAIVE BAYES MODEL WITH EXACT PARAMETERS %%%
 theta_0 = 0.35;     % P(C=0)
 phi_0 = 0.21;       % P(F=0 | C = 0)
@@ -39,20 +42,21 @@ loglikelihoodMartingale(1:length(data)) = 0;
 loglikelihoodSufficient(1:length(data)) = 0;
 loglikelihoodHybrid(1:length(data)) = 0;
 for j = 1 : length(data)
-    if (j<100)
-        [alpha, beta, gamma] = momentMatchingSufficient(alpha, beta, gamma, data(j));
-    else
-        [alpha, beta, gamma] = momentMatchingMartingale(alpha, beta, gamma, data(j));
-    end
-%     [alphaM, betaM, gammaM] = momentMatchingMartingale(alphaM, betaM, gammaM, data(j));
+%     if (j<100)
+% %         disp('BMM using sufficient moments')
+%         [alpha, beta, gamma] = momentMatchingSufficient(alpha, beta, gamma, data(j));
+%     else
+%         [alpha, beta, gamma] = momentMatchingMartingale(alpha, beta, gamma, data(j));
+%     end
+    [alphaM, betaM, gammaM] = momentMatchingMartingale(alphaM, betaM, gammaM, data(j));
     [alphaS, betaS, gammaS] = momentMatchingSufficient(alphaS, betaS, gammaS, data(j));
-%     loglikelihoodMartingale(j) = calculateloglik(alphaM(1)/sum(alphaM), betaM(1)/sum(betaM), gammaM(1)/sum(gammaM), data);
+    loglikelihoodMartingale(j) = calculateloglik(alphaM(1)/sum(alphaM), betaM(1)/sum(betaM), gammaM(1)/sum(gammaM), data);
     loglikelihoodSufficient(j) = calculateloglik(alphaS(1)/sum(alphaS), betaS(1)/sum(betaS), gammaS(1)/sum(gammaS), data);
     loglikelihoodHybrid(j) = calculateloglik(alpha(1)/sum(alpha), beta(1)/sum(beta), gamma(1)/sum(gamma), data);
 end
 
-% plot(1:length(data), loglikelihoodMartingale, '--r','Linewidth', 2);
-plot(1:length(data), loglikelihoodHybrid, '-r','Linewidth', 2);
+plot(1:length(data), loglikelihoodMartingale, '--r','Linewidth', 2);
+% plot(1:length(data), loglikelihoodHybrid, '-r','Linewidth', 2);
 hold on; plot(1:length(data), loglikelihoodSufficient, 'b','Linewidth', 2)
 hold on; plot(1:length(data), repmat(loglikelihoodData,length(data), 1), '-k', 'LineWidth',2)
 xlabel('Number of Observations')
--- momentMatchingMartingale.m
+++ momentMatchingMartingale.m
@@ -1,33 +1,22 @@
 function [alpha, beta, gamma] = momentMatchingMartingale(alpha, beta, gamma, data)
-
-
+% dbstop in momentMatchingMartingale at 76 if (alpha<0)
+% dbstop in momentMatchingMartingale at 76 if (beta<0)
+% dbstop in momentMatchingMartingale at 76 if (gamma<0)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% MOMENTS OF A DIRICHLET %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% FIRST ORDER MOMENTS %%%
 momentTheta(1) = alpha(1)/sum(alpha); 
 momentTheta(2) = alpha(1).*(alpha(1)+1)/(sum(alpha)*(1+sum(alpha)));
 momentTheta(3) = alpha(1).*(alpha(1)+1).*(alpha(1)+2)/(sum(alpha)*(1+sum(alpha))*(2+sum(alpha)));
-
+%%% SECOND ORDER MOMENTS %%%
 momentPhi(1) = beta(1)/sum(beta); 
 momentPhi(2) = beta(1).*(beta(1)+1)/(sum(beta)*(1+sum(beta)));
 momentPhi(3) = beta(1).*(beta(1)+1).*(beta(1)+2)/(sum(beta)*(1+sum(beta))*(2+sum(beta)));
-
+%%% THIRD ORDER MOMENTS %%%
 momentLambda(1) = gamma(1)/sum(gamma); 
 momentLambda(2) = gamma(1).*(gamma(1)+1)/(sum(gamma)*(1+sum(gamma)));
 momentLambda(3) = gamma(1).*(gamma(1)+1).*(gamma(1)+2)/(sum(gamma)*(1+sum(gamma))*(2+sum(gamma)));
-
-% T = alpha/sum(alpha);
-% P = beta/sum(beta);
-% L = gamma/sum(gamma);
-% 
-% %%% SECOND ORDER MOMENTS %%%
-% T2 = alpha.*(alpha+1)/(sum(alpha)*(1+sum(alpha)));
-% P2 = beta.*(beta+1)/(sum(beta)*(1+sum(beta)));
-% L2 = gamma.*(gamma+1)/(sum(gamma)*(1+sum(gamma)));
-% 
-% %%% THIRD ORDER MOMENTS %%%
-% T3 = alpha.*(alpha+1).*(alpha+2)/(sum(alpha)*(1+sum(alpha))*(2+sum(alpha)));
-% P3 = beta.*(beta+1).*(beta+2)/(sum(beta)*(1+sum(beta))*(2+sum(beta)));
-% L3 = gamma.*(gamma+1).*(gamma+2)/(sum(gamma)*(1+sum(gamma))*(2+sum(gamma)));
-
 if (data == 0)
     %%% Normalization Constant P(F=0) %%%
     Z = momentTheta(1)*momentPhi(1) + momentLambda(1) - momentTheta(1)*momentLambda(1);
@@ -84,68 +73,5 @@ else
     alpha = theta(:,1).*(theta(:,1) - theta(:,2))./(theta(:,2) - theta(:,1).^2);
     beta = phi(:,1).*(phi(:,1) - phi(:,2))./(phi(:,2) - phi(:,1).^2);
     gamma = lambda(:,1).*(lambda(:,1) - lambda(:,2))./(lambda(:,2) - lambda(:,1).^2);
-    
-    %         theta(1,1) = (T2(1)*P(1) + alpha(1)*alpha(2)*L(1)/( sum(alpha)*(sum(alpha)+1) ))/Z;
-    %         theta(2,1) = (T2(2)*L(1) + alpha(1)*alpha(2)*P(1)/( sum(alpha)*(sum(alpha)+1) ))/Z;
-    %         %%% MOMENTS TO MATCH FOR A MARTINGALE : { theta1_phi1, theta2_lambda1 }
-    %
-    %         theta1_phi1 = ( T2(1)*momentPhi(2) + ( alpha(1)*alpha(2)*P(1)*L(1) )/( sum(alpha)*(1+sum(alpha)) ) )/Z;
-    %         theta2_lambda1 = ( T2(2)*momentLambda(2) + ( alpha(1)*alpha(2)*P(1)*L(1) )/( sum(alpha)*(1+sum(alpha)) ) )/Z;
-    %
-    %         %%% REST OF THE SUFFICIENT MOMENTS
-    %                 phi(1,1) = T(1)*momentPhi(2) + T(2)*P(1)*L(1);
-    % %%%         phi(1,1) = theta1_phi1/theta(1,1);
-    %         phi(1,2) = T(1)*momentPhi(3) + T(2)*momentPhi(2)*L(1);
-    %         phi(2,1) = ( T(1)*beta(1)*beta(2) )/( sum(beta)*(1+sum(beta)) ) + T(2)*L(1)*P(2);
-    %         phi(2,2) = ( T(1)*beta(1)*beta(2)*(beta(2)+1) )/( sum(beta)*(1+sum(beta))*(2+sum(beta)) ) + T(2)*L(1)*(P2(2));
-    %         phi = phi/Z;
-    %
-    %                 lambda(1,1) = T(1)*P(1)*L(1) + T(2)*momentLambda(2);
-    % %%%         lambda(1,1) = theta2_lambda1/theta(2,1);
-    %         lambda(1,2) = T(1)*P(1)*momentLambda(2) + T(2)*momentLambda(3);
-    %         lambda(2,1) = T(1)*P(1)*L(2) + ( T(2)*gamma(1)*gamma(2) )/( sum(gamma)*(1+sum(gamma)) );
-    %         lambda(2,2) = T(1)*P(1)*L2(2) + ( T(2)*gamma(1)*gamma(2)*(gamma(2)+1) )/( sum(gamma)*(1+sum(gamma))*(2+sum(gamma)) );
-    %         lambda = lambda/Z;
-    %
-    %         %%%         theta(1,1) = theta1_phi1/phi(1,1);
-    %         %%%         theta(2,1) = theta2_lambda1/lambda(1,1);
-    %
-    %         theta(1,2) = ( momentTheta(3)*P(1) + L(1)*alpha(1)*alpha(2)*(alpha(1)+1)/(sum(alpha)*(1+sum(alpha))*(2+sum(alpha))) )/Z;
-    %         theta(2,2) = ( ( P(1)*alpha(1)*alpha(2)*(alpha(2)+1) )/( sum(alpha)*(1+sum(alpha))*(2+sum(alpha)) ) + T3(2)*L(1) )/Z;
-    %         %%%         theta = theta/Z;
-    %         %%%% PROJECTION %%%%
-    %         alpha = theta(:,1).*(theta(:,1) - theta(:,2))./( theta(:,2) - theta(:,1).^2 );
-    %         beta = phi(:,1).*(phi(:,1) - phi(:,2))./( phi(:,2) - phi(:,1).^2 );
-    %         gamma = lambda(:,1).*(lambda(:,1) - lambda(:,2))./( lambda(:,2) - lambda(:,1).^2 );
-    %     else
-    %         Z = T(1)*P(2) + T(2)*L(2);
-    %         theta(1,1) = ( T2(1)*P(2) + alpha(1)*alpha(2)*L(2)/( sum(alpha)*(sum(alpha)+1) ))/Z;
-    %         theta(2,1) = (T2(2)*L(2) + alpha(1)*alpha(2)*P(2)/( sum(alpha)*(sum(alpha)+1) ))/Z;
-    %         %%% MOMENTS TO MATCH FOR A MARTINGALE : { theta1_phi2, theta2_lambda2 }
-    %         theta1_phi2 = ( T2(1)*P2(2) + ( alpha(1)*alpha(2)*P(2)*L(2) )/( sum(alpha)*(1+sum(alpha)) ) )/Z;
-    %         theta2_lambda2 = ( T2(2)*L2(2) + ( alpha(1)*alpha(2)*P(2)*L(2) )/( sum(alpha)*(1+sum(alpha)) ) )/Z;
-    %         %%% REST OF THE SUFFICIENT MOMENTS
-    %         phi(1,1) = ( T(1)*beta(1)*beta(2) )/( sum(beta)*(1+sum(beta)) ) + T(2)*P(1)*L(2);
-    %         phi(1,2) = ( T(1)*beta(1)*beta(2)*(beta(1)+1) )/( sum(beta)*(1+sum(beta))*(2+sum(beta)) ) + T(2)*momentPhi(2)*L(2);
-    %         phi(2,1) = T(1)*P2(2) + T(2)*L(2)*P(2);
-    % %%%%         phi(2,1) = theta1_phi2/theta(1,1);
-    %         phi(2,2) = T(1)*P3(2) + T(2)*L(2)*P2(2);
-    %         phi = phi/Z;
-    %
-    %         lambda(1,1) = T(1)*P(2)*L(1) + ( T(2)*gamma(1)*gamma(2) )/( sum(gamma)*(1+sum(gamma)) );
-    %         lambda(1,2) = T(1)*P(2)*momentLambda(2) + ( T(2)*gamma(1)*gamma(2)*(gamma(1)+1) )/( sum(gamma)*(1+sum(gamma))*(2+sum(gamma)) );
-    %         lambda(2,1) = T(1)*P(2)*L(2) + T(2)*L2(2);
-    % %%%         lambda(2,1) = theta2_lambda2/theta(2,1);
-    %         lambda(2,2) = T(1)*P(2)*L2(2) + T(2)*L3(2);
-    %         lambda = lambda/Z;
-    %
-    %         %%%         theta(1,1) = theta1_phi2/phi(2,1);
-    %         %%%         theta(2,1) = theta2_lambda2/lambda(2,1);
-    %
-    %         theta(1,2) = ( momentTheta(3)*P(2) + ( L(2)*alpha(1)*alpha(2)*(alpha(1)+1) )/( sum(alpha)*(1+sum(alpha))*(2+sum(alpha)) ) )/Z;
-    %         theta(2,2) = ( ( P(2)*alpha(1)*alpha(2)*(alpha(2)+1) )/( sum(alpha)*(1+sum(alpha))*(2+sum(alpha)) ) + T3(2)*L(2) )/Z;
-    %%%% PROJECTION %%%%
-    %         alpha = theta(:,1).*(theta(:,1) - theta(:,2))./( theta(:,2) - theta(:,1).^2 );
-    %         beta = phi(:,1).*(phi(:,1) - phi(:,2))./( phi(:,2) - phi(:,1).^2 );
-    %         gamma = lambda(:,1).*(lambda(:,1) - lambda(:,2))./( lambda(:,2) - lambda(:,1).^2 );
+    gamma;
 end
